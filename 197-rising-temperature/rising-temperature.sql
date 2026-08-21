a9bc62d4a5cd7388479faba1e08c0bec85b6b790
CHADYA VATSA <chadyavatsa123@gmail.com>
2026-08-21 17:29:21 +0530

    Time: 461 ms (79.21%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT w1.id
 FROM Weather w1 INNER JOIN Weather w2
-WHERE DATEDIFF(w1.recordDate,w2.recordDate)=1 AND
+WHERE SUBDATE(w1.recordDate,1)=w2.recordDate AND
 w1.temperature > w2.temperature
