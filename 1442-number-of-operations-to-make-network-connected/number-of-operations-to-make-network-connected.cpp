84ed3c05fef11e634e586d3c354b4ec925fa39e4
CHADYA VATSA <chadyavatsa123@gmail.com>
2026-08-25 21:28:44 +0530

    Time: 2 ms (86.66%) | Memory: 42.2 MB (71.10%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,47 @@
+class DisjoinSet{
+    vector<int>parent,size;
+    public:
+    DisjoinSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int findUpar(int node){
+        if(node==parent[node])
+        return node;
+        return parent[node]=findUpar(parent[node]);
+    }
+    void unionBySize(int u,int v){
+        int ulp_u=findUpar(u);
+        int ulp_v=findUpar(v);
+        if(ulp_u==ulp_v)return;
+        else if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjoinSet ds(n);
+        if(connections.size()<n-1)return -1;
+        for(int i=0;i<connections.size();i++){
+            int u=connections[i][0];
+            int v=connections[i][1];
+            ds.unionBySize(u,v);
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(ds.findUpar(i)==i)
+              cnt++;
+        }
+        return cnt-1;
+    }
+};
