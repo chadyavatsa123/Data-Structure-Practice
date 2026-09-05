30921ca34a3b800a5a7ea0bb7f5624b0f70da768
CHADYA VATSA <chadyavatsa123@gmail.com>
2026-09-05 17:00:51 +0530

    Time: 11 ms (57.63%) | Memory: 202.5 MB (85.02%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int>mini(nums.size());
+        int mn=INT_MAX;
+        for(int i=nums.size()-1;i>=0;i--){
+           // mini[i]=min(mini[i],nums[i]);
+         if(mn>nums[i]){
+            mn=nums[i];
+         }
+         mini[i]=mn;
+        }
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+          maxi=max(maxi,nums[i]);
+          int diff=maxi-mini[i];
+          if(diff<=k)return i;
+        }
+        return -1;
+    }
+};
