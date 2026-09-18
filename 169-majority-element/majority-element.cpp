d7f3c2d8c3d32f7ba8c002b296df2b48d87e334c
CHADYA VATSA <chadyavatsa123@gmail.com>
2026-09-18 17:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.03%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+        for(auto it:mpp){
+            if(it.second>nums.size()/2)
+            return it.first;
+        }
+        return -1;
+    }
+};
